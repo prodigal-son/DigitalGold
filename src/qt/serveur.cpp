d861af6dfa24129287972077c7d965188447e75c
Crestington <Zelus2009@hotmail.com>
2015-01-31 01:54:37 -0800

    IRC changed to chat.freenode.net

--- src/qt/serveur.cpp
+++ src/qt/serveur.cpp
@@ -54,7 +54,7 @@ void Serveur::connected()
 
 	sendData("USER "+pseudo+" localhost "+serveur+" :"+pseudo);
     sendData("NICK "+pseudo);
-    affichage->append("Connected to irc.freenode.net");
+    affichage->append("Connected to chat.freenode.net");
 
 }
 
