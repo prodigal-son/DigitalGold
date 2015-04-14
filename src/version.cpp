105f0188e5f084849475f7768ee43923d28a309b
Crestington <Zelus2009@hotmail.com>
2015-04-14 11:41:07 -0700

    Bump Version

--- src/version.cpp
+++ src/version.cpp
@@ -12,7 +12,7 @@
 const std::string CLIENT_NAME("PayCon");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   ""
+#define CLIENT_VERSION_SUFFIX   "PayCon-1.0.5"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
