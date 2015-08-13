cd05d3e98d7475ecd50102f472e23da9c99affad
Crestington <Zelus2009@hotmail.com>
2015-08-13 16:51:48 -0700

    Add more .onion nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1229,6 +1229,23 @@ static const char *strMainNetOnionSeed[][1] = {
 	{"xnmbxhbbhngvp5ea.onion"},
 	{"4rvjnfn5wiyk2aqp.onion"},
 	{"sun6z3scsrloydgf.onion"},
+	{"rgz3eevnrwjvnozm.onion"},
+	{"xuzhdunw6pm2cnxo.onion"},
+	{"3tkpjldbbidxwfv3.onion"},
+	{"pjxv27nvd4ce32xb.onion"},
+	{"as6l375o37zaif3w.onion"},
+	{"v2s6pz5ipuey457f.onion"},
+	{"62jhprfz3abtkvbe.onion"},
+	{"keqtj27fa6hn3xu7.onion"},
+	{"hlgv4zokymhz26tx.onion"},
+	{"dfuwr2vvbxm6dx3k.onion"},
+	{"4363xyabb3hkjocd.onion"},
+	{"tfxfu7yxe5dkauwf.onion"},
+	{"xnmbxhbbhngvp5ea.onion"},
+	{"ev6uqghx5jjfotep.onion"},
+	{"ndawxkpfmjgbmycd.onion"},
+	{"yey3rnjdsdc77rrp.onion"},
+	{"j7dsfqsdt6pjedvq.onion"},
 
     {NULL}
 };
